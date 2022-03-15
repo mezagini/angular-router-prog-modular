fee4a5435965740ec7a0600566db1ae24e646ac0
Daniel Meza <danielmeza.4c@gmail.com>
2022-03-14 22:45:25 -0700

    producto e img components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImgComponent } from './components/img/img.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
