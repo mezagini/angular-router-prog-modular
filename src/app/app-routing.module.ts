f18d28d92e04f0661f64d2cf0713bea36f54ba9d
Daniel Meza <danielmeza.4c@gmail.com>
2022-03-26 02:30:36 -0700

    manejar ruta de error Not Found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,6 +22,7 @@ const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'recovery', component: RecoveryComponent },
   { path: 'register', component: RegisterComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
